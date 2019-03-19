c99027f4bf4fe1e326296fd9769588e9cecd6cc1
Turgay Can <Turgay.can@yandex.com.tr>
2019-03-19 20:32:50 +0300

    Update NotifySmsReception.asmx.cs

--- NotifySmsReception.asmx.cs
+++ NotifySmsReception.asmx.cs
@@ -6,7 +6,7 @@ using System.Web.Services.Protocols;
 
 namespace pimsnotifservice
 {
-    [WebService(Namespace = "http://www.avea.com.tr/pims-partner/schema/smsNotification")]
+    [WebService(Namespace = "http://www.xxxx.com.tr/pims-partner/schema/smsNotification")]
     public class NotifySmsReception : WebService
     {
 
